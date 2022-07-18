eb2cc26238b4821baa2fd2219c078f5c2388bd74
Antoine Amend <antoine.amend@gmail.com>
2022-07-18 13:16:44 -0700

    Upgrading legend-engine

--- python/tests/tests.py
+++ python/tests/tests.py
@@ -22,7 +22,7 @@ class LegendTest(unittest.TestCase):
         self.spark_conf = spark_conf
 
         # inject scala classes
-        self.spark = SparkSession.builder.appName("geoscan") \
+        self.spark = SparkSession.builder.appName("legend") \
             .config("spark.driver.extraClassPath", spark_conf) \
             .master("local") \
             .getOrCreate()
@@ -33,25 +33,61 @@ class LegendTest(unittest.TestCase):
     def test_schema(self):
         legend = LegendFileLoader().loadResources(self.legend_path)
         schema = legend.get_schema('databricks::mapping::employee_delta')
-        self.assertTrue(len(schema.fields) == 8)
+        fields = set([f.name for f in schema.fields])
+        expected = set([
+            'firstName',
+            'lastName',
+            'birthDate',
+            'gender',
+            'id',
+            'sme',
+            'joinedDate',
+            'highFives'
+        ])
+        self.assertEqual(fields, expected)
         print(schema)
 
     def test_expectations(self):
         legend = LegendFileLoader().loadResources(self.legend_path)
         expectations = legend.get_expectations('databricks::mapping::employee_delta')
-        self.assertTrue(len(expectations) == 8)
+        expected = {
+            '[birthDate] is mandatory': 'birth_date IS NOT NULL',
+            '[sme] not allowed value': "(sme IS NULL OR sme IN ('Scala', 'Python', 'Java', 'R', 'SQL'))",
+            '[id] is mandatory': 'id IS NOT NULL',
+            '[joinedDate] is mandatory': 'joined_date IS NOT NULL',
+            '[firstName] is mandatory': 'first_name IS NOT NULL',
+            '[high five] should be positive': '(high_fives IS NOT NULL AND high_fives > 0)',
+            '[lastName] is mandatory': 'last_name IS NOT NULL',
+            '[hiringAge] should be > 18': 'year(joined_date) - year(birth_date) > 18'
+        }
+        self.assertEqual(expectations, expected)
         print(expectations)
 
     def test_transformations(self):
         legend = LegendFileLoader().loadResources(self.legend_path)
         transformations = legend.get_transformations('databricks::mapping::employee_delta')
-        self.assertTrue(len(transformations) == 8)
+        expected = {
+            'highFives': 'high_fives',
+            'joinedDate': 'joined_date',
+            'lastName': 'last_name',
+            'firstName': 'first_name',
+            'birthDate': 'birth_date',
+            'id': 'id',
+            'sme': 'sme',
+            'gender': 'gender'
+        }
+        self.assertEqual(transformations, expected)
         print(transformations)
 
     def test_derivations(self):
         legend = LegendFileLoader().loadResources(self.legend_path)
         derivations = legend.get_derivations('databricks::mapping::employee_delta')
-        self.assertTrue(len(derivations) == 2)
+        expected = {
+            'hiringAge': 'year(joined_date) - year(birth_date) AS `hiringAge`',
+            'age': 'year(current_date) - year(birth_date) AS `age`',
+            'initials': "concat(substring(first_name, 0, 1), '', substring(last_name, 0, 1)) AS `initials`"
+        }
+        self.assertEqual(derivations, expected)
         print(derivations)
 
     def test_table(self):
@@ -60,6 +96,5 @@ class LegendTest(unittest.TestCase):
         self.assertTrue(table == 'legend.employee')
 
 
-## MAIN
 if __name__ == '__main__':
     unittest.main()
